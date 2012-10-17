e7ce4e18513ff021c405969aa4e31096a198799f
Tristan Jones <tristanjones@berkeley.edu>
2012-10-16 21:13:27 -0400

    Added matmul file

---
+++ sgemm-small.c
@@ -0,0 +1,11 @@
+/* CS61C Project 3: Matrix Multiply Parallelization 
+   Jian Wei Leong : cs61c-
+   Tristan Jones  : cs61c-du 
+*/
+
+#include <emmintrin.h> 
+
+void sgemm( int m, int n, float *A, float *C )
+{
+  
+}
