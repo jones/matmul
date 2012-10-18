4338c2e241e57cdd74713d6f54b75ebc7be9d5be
Tristan Jones <tristanjones@berkeley.edu>
2012-10-18 03:11:55 -0400

    found a new transpose algorithm

--- sgemm-small.c
+++ sgemm-small.c
@@ -11,15 +11,18 @@ static float tnybuffer[16 *  64] __attribute__((aligned(0x1000)));
 static float smlbuffer[16 * 128] __attribute__((aligned(0x1000)));
 static float medbuffer[16 * 192] __attribute__((aligned(0x1000)));
 static float hugbuffer[16 * 300] __attribute__((aligned(0x1000)));
-
-
-void sgemm( int m, int n, float *A, float *C )
+static float transbuff[100 * 300 * sizeof(float)] __attribute__((aligned(0x1000))); 
+// m is height, n is width
+void sgemm( int m, int n, float* __restrict A, float* __restrict C )
 {
   // Zero out C because it may be filled with garbage
+  // TODO: possibly this is not needed?
   memset(C, 0, n * m * sizeof(float));
 
+  // First compute A transpose
 
+  
+  
 
-  // First copy A into a buffer
 
 }
