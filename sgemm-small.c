b9af50fd5da4b326801d43e89f343381dc89a9ae
Tristan Jones <tristanjones@berkeley.edu>
2012-10-18 03:25:41 -0400

    Wrote a very bad transpose. Only debugged once

--- sgemm-small.c
+++ sgemm-small.c
@@ -6,20 +6,37 @@
 #include <string.h>
 #include <emmintrin.h> 
 
+#define ROWS m
+#define COLS n
+
 // Declare static buffer arrays
 static float tnybuffer[16 *  64] __attribute__((aligned(0x1000)));
 static float smlbuffer[16 * 128] __attribute__((aligned(0x1000)));
 static float medbuffer[16 * 192] __attribute__((aligned(0x1000)));
 static float hugbuffer[16 * 300] __attribute__((aligned(0x1000)));
-static float transbuff[100 * 300 * sizeof(float)] __attribute__((aligned(0x1000))); 
-// m is height, n is width
+static float transbuff[100 * 300 * sizeof(float)] __attribute__((aligned(0x1000)));
+
+
+#pragma GCC optimize (3,"unroll-all-loops")
 void sgemm( int m, int n, float* __restrict A, float* __restrict C )
 {
   // Zero out C because it may be filled with garbage
   // TODO: possibly this is not needed?
   memset(C, 0, n * m * sizeof(float));
 
-  // First compute A transpose
+  // Write A transpose into the giant buffer
+  // TODO: do this better
+  // TODO: add padding
+
+  for (int x_t = 0; x_t < COLS; x_t++) {
+    for (int y_t = 0; y_t < ROWS; y_t++) {
+      *(transbuff + y_t * COLS + x_t) = *(A + x_t * ROWS + y_t);
+    }
+  }
+
+  // Now do a bad matrix multiply
+  
+
 
   
   
