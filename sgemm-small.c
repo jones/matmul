3b5e17745627cfbee890c1f204e3e13959fcc069
Tristan Jones <tristanjones@berkeley.edu>
2012-10-18 08:00:35 -0400

    Implemented transpose. Matmul with transpose now works.

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,52 +1,34 @@
-/* CS61C Project 3: Matrix Multiply Parallelization 
-   Jian Wei Leong : cs61c-
-   Tristan Jones  : cs61c-du 
-*/
-
-#include <string.h>
+nclude <string.h>
 #include <emmintrin.h> 
 
 #define ROWS m
 #define COLS n
 
-// Declare static buffer arrays
-static float tnybuffer[16 *  64] __attribute__((aligned(0x1000)));
-static float smlbuffer[16 * 128] __attribute__((aligned(0x1000)));
-static float medbuffer[16 * 192] __attribute__((aligned(0x1000)));
-static float hugbuffer[16 * 300] __attribute__((aligned(0x1000)));
-static float transbuff[100 * 300 * sizeof(float)] __attribute__((aligned(0x1000)));
+static float transbuff[1024 * 1024] __attribute__((aligned(0x1000)));
 
 
 #pragma GCC optimize (3,"unroll-all-loops")
 void sgemm( int m, int n, float* __restrict A, float* __restrict C )
 {
-  // Zero out C because it may be filled with garbage
-  // TODO: possibly this is not needed?
+  // We should zero C out to save time
   memset(C, 0, n * m * sizeof(float));
-  memset(transbuff, 0, n * m * sizeof(float));
-
-  // Write A transpose into the giant buffer
-  // TODO: do this better
-  // TODO: add padding
-
-  for (int x_t = 0; x_t < COLS; x_t++) {
-    for (int y_t = 0; y_t < ROWS; y_t++) {
-      *(transbuff + y_t * COLS + x_t) = *(A + x_t * ROWS + y_t);
+  
+  // Write the (entire) array into the transpose buffer
+  // TODO: this will overflow on large arrays
+  for( int x_t = 0; x_t < COLS; x_t++) {
+    for( int y_t = 0; y_t < ROWS; y_t++) {
+      *(transbuff + COLS* y_t + x_t) = *(A + x_t*ROWS + y_t);
     }
   }
-
-  // Now do a bad matrix multiply
-  for (int x = 0; i < COLS; x++) {
-    for (int y = 0; y < ROWS; y++) {
-      for (int i = 0; j < ROWS; i++) {
-        C[x + i * ROWS] += (*(transbuff + y*COLS + x)) * (*(transbuff + y*COLS + i));
+        
+  // Mulitply the matrix
+  // The good thing about this is that performance doesnt degrade as quickly
+  for (int y = 0; y < ROWS; y++){
+    for (int x = 0; x < ROWS; x++) {
+      for (int i = 0; i < COLS; i++) {
+        *(C + x * ROWS + y) += *(transbuff + COLS * y + i) * *(transbuff + COLS * x + i);
       }
     }
   }
-
-
-  
-  
-
 
 }
