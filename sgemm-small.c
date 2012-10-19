46fdd6ad6f0e4bf14c8167b7c8eae59e25a3ebf9
Tristan Jones <tristanjones@berkeley.edu>
2012-10-19 04:28:34 -0400

    Rewrote code using cache lines and SSE. Currently at 7.5 Gflops

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,4 +1,4 @@
-nclude <string.h>
+#include <string.h>
 #include <emmintrin.h> 
 
 #define ROWS m
@@ -7,28 +7,43 @@ nclude <string.h>
 static float transbuff[1024 * 1024] __attribute__((aligned(0x1000)));
 
 
-#pragma GCC optimize (3,"unroll-all-loops")
+#pragma GCC optimize (2,"unroll-all-loops", "fast-math")
 void sgemm( int m, int n, float* __restrict A, float* __restrict C )
 {
-  // We should zero C out to save time
-  memset(C, 0, n * m * sizeof(float));
+  // We should zero C because it could cause problems
+  memset(C, 0, (n +n%4) * (m+m%4) * sizeof(float));
   
-  // Write the (entire) array into the transpose buffer
-  // TODO: this will overflow on large arrays
-  for( int x_t = 0; x_t < COLS; x_t++) {
-    for( int y_t = 0; y_t < ROWS; y_t++) {
-      *(transbuff + COLS* y_t + x_t) = *(A + x_t*ROWS + y_t);
-    }
+   // Fast pointers
+  float* __restrict vptra = A;
+  float* __restrict vptrc = C;
+  
+  __m128 va1   =  _mm_setzero_ps();
+  __m128 va2   =  _mm_setzero_ps();
+  __m128 vc1   =  _mm_setzero_ps();
+  __m128 vc2   =  _mm_setzero_ps();
+  __m128 vscal =  _mm_setzero_ps(); // Scalar
+  
+  for (int x = 0; x < COLS; x++) {
+    vptra = A + x*ROWS;
+    for (int y = 0; y < ROWS; y++) {
+	vptrc = C + y*ROWS;
+	vscal = _mm_load1_ps(A + y + x*ROWS);
+      for (int i = 0; i < ROWS; i+=8) { 
+		
+		va1 = _mm_loadu_ps(vptra + i);
+		vc1 = _mm_loadu_ps(vptrc + i);
+		va1 = _mm_mul_ps(va1, vscal);
+		va2 = _mm_loadu_ps(vptra + i + 4);
+		vc1 = _mm_add_ps(vc1, va1);
+		vc2 = _mm_loadu_ps(vptrc + i + 4);	
+        _mm_storeu_ps(vptrc + i, vc1);		
+		va2 = _mm_mul_ps(va2, vscal);
+		vc2 = _mm_add_ps(vc2, va2);
+		_mm_storeu_ps(vptrc + i + 4, vc2);
+	  }
+	}
   }
-        
-  // Mulitply the matrix
-  // The good thing about this is that performance doesnt degrade as quickly
-  for (int y = 0; y < ROWS; y++){
-    for (int x = 0; x < ROWS; x++) {
-      for (int i = 0; i < COLS; i++) {
-        *(C + x * ROWS + y) += *(transbuff + COLS * y + i) * *(transbuff + COLS * x + i);
-      }
-    }
-  }
+   
 
+  
 }
