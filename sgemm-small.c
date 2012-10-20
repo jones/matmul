9a7e9d7ef723177a4ef25f89479a7991c7129ff0
Tristan Jones <tristanjones@berkeley.edu>
2012-10-20 06:50:57 -0400

    Runs 60x60 at 10.9, passes random matricie test. Still need login though.

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,57 +1,105 @@
-#include <string.h>
-#include <emmintrin.h> 
+/* CS61C Project 3: Matrix Multiply Parallelization 
+   Jian Wei Leong : cs61c-
+   Tristan Jones  : cs61c-du 
+*/
 
-#define ROWS m
-#define COLS n
+#include <emmintrin.h>
 
-//#pragma GCC optimize (2,"unroll-all-loops", "fast-math","unsafe-loop-optimizations")
-void sgemm( int m, int n, float* __restrict A, float* __restrict C )
+void sgemm( int m, int n, float *A, float *C )
 {
-  // Fast pointers
-  float* __restrict vptra = A;
-  float* __restrict vptrc = C;
-  
-  __m128 va1, va2, va3, va4, va5, va6;
-  __m128 vc1, vc2, vc3;
-  __m128 vs1, vs2;  
-  
-  for (int x = 0; x < COLS; x+=2) {
-    for (int y = 0; y < ROWS; y++) {
-	vs1 = _mm_load1_ps(A + y + ROWS*x);
-	vs2 = _mm_load1_ps(A + y + ROWS*(x+1));
-
-      for (int i = 0; i < ROWS; i+= 12) { 
-		va1 = _mm_loadu_ps(A + x*ROWS + i);
-		va2 = _mm_loadu_ps(A + x*ROWS + i + 4);
-		va3 = _mm_loadu_ps(A + x*ROWS + i + 8);
-        va4 = _mm_loadu_ps(A + ROWS*(x+1) + i);
-		va5 = _mm_loadu_ps(A + ROWS*(x+1) + i + 4);
-		va6 = _mm_loadu_ps(A + ROWS*(x+1) + i + 8);
-
-		vc1 = _mm_loadu_ps(C + y*ROWS + i);
-		vc2 = _mm_loadu_ps(C + y*ROWS + i + 4);
-		vc3 = _mm_loadu_ps(C + y*ROWS + i + 8);
-
-		va1 = _mm_mul_ps(va1, vs1);
-		va2 = _mm_mul_ps(va2, vs1);
-		va3 = _mm_mul_ps(va3, vs1);
-		va4 = _mm_mul_ps(va4, vs2);
-		va5 = _mm_mul_ps(va5, vs2);
-		va6 = _mm_mul_ps(va6, vs2);
-
-		vc1 = _mm_add_ps(vc1, va1);
-		vc1 = _mm_add_ps(vc1, va4);
-		vc2 = _mm_add_ps(vc2, va2);
-		vc2 = _mm_add_ps(vc2, va5);
-		vc3 = _mm_add_ps(vc3, va3);
-		vc3 = _mm_add_ps(vc3, va6);
-
-		_mm_storeu_ps(C + y*ROWS + i, vc1);
-		_mm_storeu_ps(C + y*ROWS + i + 4, vc2);
-		_mm_storeu_ps(C + y*ROWS + i + 8, vc3);
-	  }
-	}
+  int i,j,k,ii;
+  int mbtm = (m/12)*12;
+  int nbtm = (n/4)*4;
+  float* cptr;
+  float* aptr1;
+  float* aptr2;
+  float* aptr3;
+  float* aptr4;
+  __m128 cv1, tv1, tv2, tv3, tv4;
+  __m128 bv1, av1, av2, av3, av4;
+  for (k=0; k<nbtm; k+=4){
+    for (j=0; j<m; j++){
+      tv1 = _mm_load1_ps(A+j+k*m);
+      tv2 = _mm_load1_ps(A+j+(k+1)*m);
+      tv3 = _mm_load1_ps(A+j+(k+2)*m);
+      tv4 = _mm_load1_ps(A+j+(k+3)*m);
+      for (i=0; i<mbtm; i+=12){
+        aptr1 = A+i+k*m;
+        aptr2 = A+i+(k+1)*m;
+        aptr3 = A+i+(k+2)*m;
+        aptr4 = A+i+(k+3)*m;
+        cptr = C+i+j*m;
+		
+		cv1 = _mm_loadu_ps(cptr);
+		av1 = _mm_loadu_ps(aptr1);
+		av2 = _mm_loadu_ps(aptr2);
+		av3 = _mm_loadu_ps(aptr3);
+		av4 = _mm_loadu_ps(aptr4);
+		av1 = _mm_mul_ps(av1, tv1);
+		av2 = _mm_mul_ps(av2, tv2);
+		av3 = _mm_mul_ps(av3, tv3);
+		av4 = _mm_mul_ps(av4, tv4);
+		bv1 = _mm_add_ps(cv1, av1);
+		cv1 = _mm_add_ps(av2, av3);
+		bv1 = _mm_add_ps(bv1, av4);
+		cv1 = _mm_add_ps(bv1, cv1);
+        _mm_storeu_ps(cptr, cv1);       
+        aptr1 += 4;
+        aptr2 += 4;
+        aptr3 += 4;
+        aptr4 += 4;
+        cptr += 4;
+		
+        bv1 = _mm_loadu_ps(cptr);
+        bv1 = _mm_add_ps(bv1, _mm_mul_ps(_mm_loadu_ps(aptr1),tv1));
+        bv1 = _mm_add_ps(bv1, _mm_mul_ps(_mm_loadu_ps(aptr2),tv2));
+        bv1 = _mm_add_ps(bv1, _mm_mul_ps(_mm_loadu_ps(aptr3),tv3));
+        bv1 = _mm_add_ps(bv1, _mm_mul_ps(_mm_loadu_ps(aptr4),tv4));
+        _mm_storeu_ps(cptr, bv1);
+        aptr1 += 4;
+        aptr2 += 4;
+        aptr3 += 4;
+        aptr4 += 4;
+        cptr += 4;
+		
+        cv1 = _mm_loadu_ps(cptr);
+		av1 = _mm_loadu_ps(aptr1);
+		av2 = _mm_loadu_ps(aptr2);
+		av3 = _mm_loadu_ps(aptr3);
+		av4 = _mm_loadu_ps(aptr4);
+		av1 = _mm_mul_ps(av1, tv1);
+		av2 = _mm_mul_ps(av2, tv2);
+		av3 = _mm_mul_ps(av3, tv3);
+		av4 = _mm_mul_ps(av4, tv4);
+		bv1 = _mm_add_ps(cv1, av1);
+		cv1 = _mm_add_ps(av2, av3);
+		bv1 = _mm_add_ps(bv1, av4);
+		cv1 = _mm_add_ps(bv1, cv1);
+        _mm_storeu_ps(cptr, cv1);
+      }
+    }
   }
-
-  
+  for (k=nbtm; k<n; k++){
+    for (j=0; j<m; j++){
+      tv1 = _mm_load1_ps(A+j+k*m);
+      for (i=0; i<mbtm; i+=12){
+        aptr1 = A+i+k*m;
+        cptr = C+i+j*m;
+        _mm_storeu_ps(cptr, _mm_add_ps(_mm_loadu_ps(cptr), _mm_mul_ps(_mm_loadu_ps(aptr1),tv1)));
+        aptr1 += 4;
+        cptr += 4;
+        _mm_storeu_ps(cptr, _mm_add_ps(_mm_loadu_ps(cptr), _mm_mul_ps(_mm_loadu_ps(aptr1),tv1)));
+        aptr1 += 4;
+        cptr += 4;
+        _mm_storeu_ps(cptr, _mm_add_ps(_mm_loadu_ps(cptr), _mm_mul_ps(_mm_loadu_ps(aptr1),tv1)));
+      }
+    }
+  }
+  for (k=0; k<n; k++){
+    for (j=0; j<m; j++){
+      for (ii=i; ii<m; ii++){
+        C[ii+j*m] += A[ii+k*m] * A[j+k*m];
+      }
+    }
+  }
 }
