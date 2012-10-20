d2dc3e9d84ef42f3bb51bf1d95680eb632a22da3
Tristan Jones <tristanjones@berkeley.edu>
2012-10-20 01:44:37 -0400

    Gets 11.3 gflops; works only on matricies size 12m by 2n

--- sgemm-small.c
+++ sgemm-small.c
@@ -4,109 +4,54 @@
 #define ROWS m
 #define COLS n
 
-//static float transbuff[1024 * 1024] __attribute__((aligne d(0x1000)));
-
-
 //#pragma GCC optimize (2,"unroll-all-loops", "fast-math","unsafe-loop-optimizations")
 void sgemm( int m, int n, float* __restrict A, float* __restrict C )
 {
-   // Fast pointers
+  // Fast pointers
   float* __restrict vptra = A;
   float* __restrict vptrc = C;
   
-  __m128 va1   =  _mm_setzero_ps();
-  __m128 va2   =  _mm_setzero_ps();
-  __m128 va3   =  _mm_setzero_ps();
-  __m128 va4   =  _mm_setzero_ps();
-  
-  __m128 vc1   =  _mm_setzero_ps();
-  __m128 vc2   =  _mm_setzero_ps();
-  __m128 vc3   =  _mm_setzero_ps();
-  __m128 vc4   =  _mm_setzero_ps();
-  
-  __m128 vs1 =  _mm_setzero_ps(); // Scalar
-  __m128 vs2 =  _mm_setzero_ps(); // Scalar
-  
-  
+  __m128 va1, va2, va3, va4, va5, va6;
+  __m128 vc1, vc2, vc3;
+  __m128 vs1, vs2;  
   
   for (int x = 0; x < COLS; x+=2) {
     for (int y = 0; y < ROWS; y++) {
 	vs1 = _mm_load1_ps(A + y + ROWS*x);
 	vs2 = _mm_load1_ps(A + y + ROWS*(x+1));
-	
-      for (int i = 0; i < ROWS; i+= 8) { 
+
+      for (int i = 0; i < ROWS; i+= 12) { 
 		va1 = _mm_loadu_ps(A + x*ROWS + i);
 		va2 = _mm_loadu_ps(A + x*ROWS + i + 4);
-        va3 = _mm_loadu_ps(A + ROWS*(x+1) + i);
-		va4 = _mm_loadu_ps(A + ROWS*(x+1) + i + 4);
-		
+		va3 = _mm_loadu_ps(A + x*ROWS + i + 8);
+        va4 = _mm_loadu_ps(A + ROWS*(x+1) + i);
+		va5 = _mm_loadu_ps(A + ROWS*(x+1) + i + 4);
+		va6 = _mm_loadu_ps(A + ROWS*(x+1) + i + 8);
+
 		vc1 = _mm_loadu_ps(C + y*ROWS + i);
 		vc2 = _mm_loadu_ps(C + y*ROWS + i + 4);
-		
+		vc3 = _mm_loadu_ps(C + y*ROWS + i + 8);
+
 		va1 = _mm_mul_ps(va1, vs1);
 		va2 = _mm_mul_ps(va2, vs1);
-		va3 = _mm_mul_ps(va3, vs2);
+		va3 = _mm_mul_ps(va3, vs1);
 		va4 = _mm_mul_ps(va4, vs2);
-		
+		va5 = _mm_mul_ps(va5, vs2);
+		va6 = _mm_mul_ps(va6, vs2);
+
 		vc1 = _mm_add_ps(vc1, va1);
+		vc1 = _mm_add_ps(vc1, va4);
 		vc2 = _mm_add_ps(vc2, va2);
-		vc1 = _mm_add_ps(vc1, va3);
-		vc2 = _mm_add_ps(vc2, va4);
-		
+		vc2 = _mm_add_ps(vc2, va5);
+		vc3 = _mm_add_ps(vc3, va3);
+		vc3 = _mm_add_ps(vc3, va6);
+
 		_mm_storeu_ps(C + y*ROWS + i, vc1);
 		_mm_storeu_ps(C + y*ROWS + i + 4, vc2);
+		_mm_storeu_ps(C + y*ROWS + i + 8, vc3);
 	  }
 	}
   }
-  
-  
-  
-  
-  /*
-  
- 
-   // Fast pointers
-  float* __restrict vptra = A;
-  float* __restrict vptrc = C;
-  float* __restrict vptraTMP = A;
-  float* __restrict vptrcTMP = C;
-  
-  __m128 va1   =  _mm_setzero_ps();
-  __m128 va2   =  _mm_setzero_ps();
-  __m128 vc1   =  _mm_setzero_ps();
-  __m128 vc2   =  _mm_setzero_ps();
-  __m128 vscal =  _mm_setzero_ps(); // Scalar
-  
-  
-  
-  for (int x = 0; x < COLS; x++) {
-    vptra = A + x*ROWS;
-    for (int y = 0; y < ROWS; y++) {
-	vptrc = C + y*ROWS;
-	vscal = _mm_load1_ps(A + y + x*ROWS);
-	vptraTMP = vptra;
-	vptrcTMP = vptrc;
-      for (int i = 0; i < ROWS; i+=8) { 
-		va1 = _mm_loadu_ps(vptraTMP);
-		va2 = _mm_loadu_ps(vptraTMP + 4);
-
-		vc1 = _mm_loadu_ps(vptrcTMP);
-		vc2 = _mm_loadu_ps(vptrcTMP+ 4);
-		
-		va1 = _mm_mul_ps(va1, vscal);
-		va2 = _mm_mul_ps(va2, vscal);
-		
-		vc1 = _mm_add_ps(vc1, va1);
-		vc2 = _mm_add_ps(vc2, va2);
-		
-		_mm_storeu_ps(vptrcTMP, vc1);
-		_mm_storeu_ps(vptrcTMP + 4, vc2);
-		vptrcTMP += 8;
-		vptraTMP += 8;
-	  }
-	}
-  }
-   */
 
   
 }
