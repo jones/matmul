ef30aa1e62ad30be52c780668a139f2be59c600d
Tristan Jones <tristanjones@berkeley.edu>
2012-10-23 21:46:29 -0400

    Added the file openmp.c

---
+++ sgemm-openmp.c
@@ -0,0 +1,13 @@
+/* CS61C Project 3: Matrix Multiply Parallelization 
+   Jian Wei Leong : cs61c-sh
+   Tristan Jones  : cs61c-du 
+*/
+
+#include <emmintrin.h>
+#include <omp.h>
+
+void sgemm( int m, int n, float *A, float *C )
+{
+ // Add the rest in later
+
+}
